3dfe27eae5813c4b5a466ca314b6d974b0e8ff80
Kacper Kasztelanic <kcpr51@hotmail.com>
2018-12-21 17:32:13 +0100

    Add VehicleResource

--- src/main/java/com/kasztelanic/carcare/repository/VehicleRepository.java
+++ src/main/java/com/kasztelanic/carcare/repository/VehicleRepository.java
@@ -18,6 +18,4 @@ public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
     @Query("select vehicle from Vehicle vehicle where vehicle.id = :id and vehicle.owner.login = ?#{principal.username}")
     Optional<Vehicle> findByIdAndOwnerIsCurrentUser(@Param("id") Long id);
-
-    Optional<Vehicle> findByUuid(String uuid);
 }
--- src/main/java/com/kasztelanic/carcare/web/rest/VehicleResource.java
+++ src/main/java/com/kasztelanic/carcare/web/rest/VehicleResource.java
@@ -1,120 +1,93 @@
 package com.kasztelanic.carcare.web.rest;
 
-import com.codahale.metrics.annotation.Timed;
-import com.kasztelanic.carcare.domain.Vehicle;
-import com.kasztelanic.carcare.repository.VehicleRepository;
-import com.kasztelanic.carcare.web.rest.errors.BadRequestAlertException;
-import com.kasztelanic.carcare.web.rest.util.HeaderUtil;
-import io.github.jhipster.web.util.ResponseUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.net.URI;
-import java.net.URISyntaxException;
-
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
-/**
- * REST controller for managing Vehicle.
- */
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kasztelanic.carcare.domain.Vehicle;
+import com.kasztelanic.carcare.repository.VehicleRepository;
+import com.kasztelanic.carcare.service.dto.VehicleDto;
+import com.kasztelanic.carcare.service.mapper.VehicleMapper;
+import com.kasztelanic.carcare.web.rest.util.HeaderUtil;
+import com.kasztelanic.carcare.web.rest.util.URIUtil;
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/vehicle")
 public class VehicleResource {
-
-    private final Logger log = LoggerFactory.getLogger(VehicleResource.class);
 
     private static final String ENTITY_NAME = "vehicle";
 
+    @Autowired
     private VehicleRepository vehicleRepository;
 
-    public VehicleResource(VehicleRepository vehicleRepository) {
-        this.vehicleRepository = vehicleRepository;
+    @Autowired
+    private VehicleMapper vehicleMapper;
+
+    @Transactional
+    @PutMapping("")
+    public ResponseEntity<VehicleDto> addVehicle(@RequestBody VehicleDto vehicleDto) {
+        VehicleDto result = vehicleMapper
+                .vehicleToVehicleDto(vehicleRepository.save(vehicleMapper.vehicleDtoToVehicle(vehicleDto)));
+        return ResponseEntity.created(URIUtil.buildURI(String.format("/api/vehicle/%s", result.getId().toString())))
+                .headers(HeaderUtil.createEntityCreationAlert(ENTITY_NAME, result.getId().toString())).body(result);
     }
 
-    /**
-     * POST  /vehicles : Create a new vehicle.
-     *
-     * @param vehicle the vehicle to create
-     * @return the ResponseEntity with status 201 (Created) and with body the new vehicle, or with status 400 (Bad Request) if the vehicle has already an ID
-     * @throws URISyntaxException if the Location URI syntax is incorrect
-     */
-    @PostMapping("/vehicles")
-    @Timed
-    public ResponseEntity<Vehicle> createVehicle(@Valid @RequestBody Vehicle vehicle) throws URISyntaxException {
-        log.debug("REST request to save Vehicle : {}", vehicle);
-        if (vehicle.getId() != null) {
-            throw new BadRequestAlertException("A new vehicle cannot already have an ID", ENTITY_NAME, "idexists");
-        }
-        Vehicle result = vehicleRepository.save(vehicle);
-        return ResponseEntity.created(new URI("/api/vehicles/" + result.getId()))
-            .headers(HeaderUtil.createEntityCreationAlert(ENTITY_NAME, result.getId().toString()))
-            .body(result);
+    @Transactional
+    @PostMapping("/{id}")
+    public ResponseEntity<VehicleDto> editVehicle(@PathVariable Long id, @RequestBody VehicleDto vehicleDto) {
+        return vehicleRepository.findByIdAndOwnerIsCurrentUser(id)
+                .map(i -> updateVehicle(i, vehicleMapper.vehicleDtoToVehicle(vehicleDto))).map(vehicleRepository::save)
+                .map(vehicleMapper::vehicleToVehicleDto)
+                .map(i -> ResponseEntity.ok()
+                        .headers(HeaderUtil.createEntityUpdateAlert(ENTITY_NAME, i.getId().toString())).body(i))
+                .orElse(ResponseEntity.notFound().build());
     }
 
-    /**
-     * PUT  /vehicles : Updates an existing vehicle.
-     *
-     * @param vehicle the vehicle to update
-     * @return the ResponseEntity with status 200 (OK) and with body the updated vehicle,
-     * or with status 400 (Bad Request) if the vehicle is not valid,
-     * or with status 500 (Internal Server Error) if the vehicle couldn't be updated
-     * @throws URISyntaxException if the Location URI syntax is incorrect
-     */
-    @PutMapping("/vehicles")
-    @Timed
-    public ResponseEntity<Vehicle> updateVehicle(@Valid @RequestBody Vehicle vehicle) throws URISyntaxException {
-        log.debug("REST request to update Vehicle : {}", vehicle);
-        if (vehicle.getId() == null) {
-            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
-        }
-        Vehicle result = vehicleRepository.save(vehicle);
-        return ResponseEntity.ok()
-            .headers(HeaderUtil.createEntityUpdateAlert(ENTITY_NAME, vehicle.getId().toString()))
-            .body(result);
-    }
-
-    /**
-     * GET  /vehicles : get all the vehicles.
-     *
-     * @return the ResponseEntity with status 200 (OK) and the list of vehicles in body
-     */
-    @GetMapping("/vehicles")
-    @Timed
-    public List<Vehicle> getAllVehicles() {
-        log.debug("REST request to get all Vehicles");
-        return vehicleRepository.findAll();
-    }
-
-    /**
-     * GET  /vehicles/:id : get the "id" vehicle.
-     *
-     * @param id the id of the vehicle to retrieve
-     * @return the ResponseEntity with status 200 (OK) and with body the vehicle, or with status 404 (Not Found)
-     */
-    @GetMapping("/vehicles/{id}")
-    @Timed
-    public ResponseEntity<Vehicle> getVehicle(@PathVariable Long id) {
-        log.debug("REST request to get Vehicle : {}", id);
-        Optional<Vehicle> vehicle = vehicleRepository.findById(id);
-        return ResponseUtil.wrapOrNotFound(vehicle);
-    }
-
-    /**
-     * DELETE  /vehicles/:id : delete the "id" vehicle.
-     *
-     * @param id the id of the vehicle to delete
-     * @return the ResponseEntity with status 200 (OK)
-     */
-    @DeleteMapping("/vehicles/{id}")
-    @Timed
+    @Transactional
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteVehicle(@PathVariable Long id) {
-        log.debug("REST request to delete Vehicle : {}", id);
+        Optional<Vehicle> vehicle = vehicleRepository.findByIdAndOwnerIsCurrentUser(id);
+        if (vehicle.isPresent()) {
+            vehicleRepository.delete(vehicle.get());
+            return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert(ENTITY_NAME, id.toString()))
+                    .build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 
-        vehicleRepository.deleteById(id);
-        return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert(ENTITY_NAME, id.toString())).build();
+    @Transactional
+    @GetMapping("/all")
+    public List<VehicleDto> getAllVehicles() {
+        return vehicleRepository.findByOwnerIsCurrentUser().stream().map(vehicleMapper::vehicleToVehicleDto)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    @GetMapping("/{id}")
+    public ResponseEntity<VehicleDto> getVehicle(@PathVariable Long id) {
+        return vehicleRepository.findByIdAndOwnerIsCurrentUser(id).map(vehicleMapper::vehicleToVehicleDto)
+                .map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    private static Vehicle updateVehicle(Vehicle vehicle, Vehicle updatedVehicle) {
+        vehicle.setFuelType(updatedVehicle.getFuelType());
+        vehicle.setLicensePlate(updatedVehicle.getLicensePlate());
+        vehicle.setMake(updatedVehicle.getMake());
+        vehicle.setModel(vehicle.getModel());
+        vehicle.setVehicleDetails(updatedVehicle.getVehicleDetails());
+        return vehicle;
     }
 }
